5dd665b68f7b5ae64a9f6b40f5bc23a97345cce3
Suyash-Chandra-Sapiens <suyash.chandra@sapiens.com>
2022-12-01 11:20:45 +0530

    added MUI library, changes routes, added lock.json to gitignore

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -5,10 +5,12 @@ export default function HomePage() {
     <h1><u>Journey List</u></h1>
       <ul>
         <li>
-          <strong><Link to="/fleet">Fleet</Link></strong>
+          <strong><Link to="/fleet">fleet</Link></strong>
         </li>
         <li>
-          <strong><Link to="/HomeOwner">HomeOwner</Link></strong>
+          <strong><Link to="/HomeOwner?policyExternalNumber=PL-HMP0001078976%2F00">homeowner</Link></strong>
+          {/* <strong><Link to="/HomeOwner">HomeOwner</Link></strong> */}
+
         </li>
       </ul>
     </>
